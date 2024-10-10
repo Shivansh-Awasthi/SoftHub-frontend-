b84728c916368e18283e1e755016dc56872050a5
Shivansh <shivanshawasthi33@gmail.com>
2024-10-10 19:59:05 +0530

    registeration form

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import dotenv from "dotenv";
+import axios from 'axios';
+
+
+
 
 const Register = () => {
+
+    try {
+        const userRegister = async () => {
+            const response = await axios.post(`http://localhost:8080/api/user/signup`)
+            console.log(response);
+        }
+        useEffect(() => {
+            // userRegister();
+        }, [])
+    } catch (error) {
+
+    }
+
+
     return (
-        <div>Register</div>
+        <div>
+            <div className='container'>
+                <div className='cover'>
+                    <form action="">
+
+                        <div className='user'>
+                            <label className='form-label'>Username</label>
+                            <input type="text" placeholder='Enter your name' />
+                        </div>
+
+                        <div className='user'>
+                            <label className='form-label'>Email</label>
+                            <input type="text" placeholder='Enter your name' />
+                        </div>
+
+                        <div className='user'>
+                            <label className='form-label'>Password</label>
+                            <input type="text" placeholder='Enter your name' />
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
 
