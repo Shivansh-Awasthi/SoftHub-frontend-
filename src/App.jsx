1d90f92976372d13725bd3f8f209ef24cbb37c7e
Shivansh <shivanshawasthi33@gmail.com>
2024-10-11 19:04:22 +0530

    added home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Register from './components/Register'
+import Login from './components/Login'
+import Home from './components/Home'
+
 
 function App() {
 
@@ -6,7 +10,14 @@ function App() {
 
   return (
     <div>
-      <Register />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/signup' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/home' element={<Home />} />
+
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
