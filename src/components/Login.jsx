47b31b195cd89ddaa47b780325e422f9f2b738ec
Shivansh <shivanshawasthi33@gmail.com>
2024-10-11 19:36:28 +0530

    added login logic

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,69 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+
+
+    const handleEmail = (e) => {
+        setEmail(e.target.value)
+    }
+
+    const handlePassword = (e) => {
+        setPassword(e.target.value)
+    }
+
+
+    const handleChange = async (e) => {
+        e.preventDefault();
+        setEmail("")
+        setPassword('')
+
+        try {
+            const response = await axios.post(`${process.env.REACT_API}/user/signin`, {
+                email,
+                password
+            })
+
+            console.log(response.data.message);
+        } catch (error) {
+            console.log(error);
+
+        }
+
+
+    }
+
+
+
+
+
     return (
-        <div>Login</div>
+        <div className='container'>
+            <div className="cover">
+                <form onSubmit={handleChange}>
+                    <div>
+                        <h2>Signin</h2>
+                    </div>
+                    <div className='user'>
+                        <label className='form-label'>Email</label>
+                        <input type="email" placeholder='Enter your email' onChange={handleEmail} />
+                    </div>
+                    <div>
+                        <label className='form-label'>Password</label>
+                        <input type="password" placeholder='Enter your password' onChange={handlePassword} />
+                    </div>
+                    <div className='button'>
+                        <button type='submit'>SignIn</button>
+                    </div>
+
+                </form>
+            </div>
+
+        </div>
     )
 }
 
