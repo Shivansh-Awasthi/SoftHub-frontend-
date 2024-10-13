57c098cb881beb1aa77e6415f298d333355c2174
Shivansh <shivanshawasthi33@gmail.com>
2024-10-14 03:31:12 +0530

    added basic content

--- src/components/category/Mac.jsx
+++ src/components/category/Mac.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Mac = () => {
     return (
-        <div>Mac</div>
+        <div className='container'>
+            <div className='cover'>
+                <h1>Games</h1>
+            </div>
+            <div className='cards'>
+                <div className='card'>
+                    <div className='card-img'></div>
+                    <div className='card-info'>
+                        <div className='title'>
+                            <h3>Game Name</h3>
+                        </div>
+                        <div>Size: 40gb</div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
