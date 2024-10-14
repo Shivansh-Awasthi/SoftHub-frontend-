f2ea2c7885d28141263a77ec8c5accea31282fc0
Shivansh <shivanshawasthi33@gmail.com>
2024-10-14 16:47:53 +0530

    minor fix

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -23,7 +23,7 @@ const Login = () => {
         setPassword('')
 
         try {
-            const response = await axios.post(`${process.env.REACT_API}/user/signin`, {
+            const response = await axios.post(`${process.env.REACT_API}api/user/signin`, {
                 email,
                 password
             })
