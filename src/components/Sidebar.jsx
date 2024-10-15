e6aa8149ea693507bd5d1768b64fc347498fc6c4
Shivansh <shivanshawasthi33@gmail.com>
2024-10-16 02:36:50 +0530

    sidebar logic nd routes

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,104 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { SiRiotgames } from "react-icons/si";
+import { SiMicrosoftonedrive } from "react-icons/si";
+import { FaPlaystation } from "react-icons/fa";
+import { PiDesktopDuotone } from "react-icons/pi";
+import { TfiAndroid } from "react-icons/tfi";
+import { RiMacbookLine } from "react-icons/ri";
+import { FaChevronDown } from "react-icons/fa";
+import { FaChevronUp } from "react-icons/fa";
+
+
+
+
+
+const Sidebar = () => {
+
+    const [logo, setLogo] = useState("https://res.cloudinary.com/dkp1pshuw/image/upload/v1729024140/Screenshot_2024-10-16_at_1.54.35_AM_cow9by.png");
+    return (
+        <aside className="sticky top-0 z-10 flex flex-col w-60 h-screen px-6 py-8 overflow-y-auto border-r border-white border-opacity-5">
+            <Link
+                to="/"
+                className='flex items-center mb-6'
+                onMouseEnter={() => setLogo("https://res.cloudinary.com/dkp1pshuw/image/upload/v1729024140/Screenshot_2024-10-16_at_1.54.39_AM_gzfxsu.png")}
+                onMouseLeave={() => setLogo("https://res.cloudinary.com/dkp1pshuw/image/upload/v1729024140/Screenshot_2024-10-16_at_1.54.35_AM_cow9by.png")}
+            >
+                <img className="h-11 w-11 mr-2 p-1" src={logo} alt="Logo" />
+                <h1 className='text-xl font-semibold tracking-wider'>
+                    <span>Soft</span>
+                    <span className='text-[#8E8E8E]'>Hub</span>
+                </h1>
+            </Link>
+
+            <div className="flex flex-col justify-between flex-1">
+                <nav className="-mx-3 space-y-6">
+                    <div className="space-y-3">
+                        <label className="px-3 text-xs text-gray-500 uppercase">Games</label>
+                        <Link to="/category/pc/games" className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700">
+                            <SiRiotgames />
+                            <span className="mx-2 text-sm font-medium">PC</span>
+                        </Link>
+                        <Link to="/category/mac/games" className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700">
+                            <RiMacbookLine />
+                            <span className="mx-2 text-sm font-medium">Mac</span>
+                        </Link>
+                        <Link to="/category/android/games" className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700">
+                            <TfiAndroid />
+                            <span className="mx-2 text-sm font-medium">Android</span>
+                        </Link>
+                    </div>
+
+                    <div className="space-y-3">
+                        <label className="px-3 text-xs text-gray-500 uppercase">Softwares</label>
+                        <Link to="/category/pc/softwares" className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700">
+                            <SiRiotgames />
+                            <span className="mx-2 text-sm font-medium">PC</span>
+                        </Link>
+                        <Link to="/category/mac/softwares" className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700">
+                            <RiMacbookLine />
+                            <span className="mx-2 text-sm font-medium">Mac</span>
+                        </Link>
+                        <Link to="/category/android/softwares" className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700">
+                            <TfiAndroid />
+                            <span className="mx-2 text-sm font-medium">Android</span>
+                        </Link>
+                    </div>
+
+                    <div className="space-y-3">
+                        <label className="px-3 text-xs text-gray-500 uppercase">Playstation ISO's</label>
+                        <Link to="/category/ppsspp/iso" className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700">
+                            <FaPlaystation />
+                            <span className="mx-2 text-sm font-medium">PPSSPP</span>
+                        </Link>
+                        <Link to="/category/ps2/iso" className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700">
+                            <FaPlaystation />
+                            <span className="mx-2 text-sm font-medium">PS2</span>
+                        </Link>
+                        <Link to="/category/ps3/iso" className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700">
+                            <FaPlaystation />
+                            <span className="mx-2 text-sm font-medium">PS3</span>
+                        </Link>
+                    </div>
+                </nav>
+            </div>
+
+            <div className="mt-auto text-[#8E8E8E] text-xs flex flex-wrap space-y-0.5">
+                <Link to="#" className="hover:underline">Copyright Holders (DMCA)</Link>
+                <span className="mx-1"> , </span>
+                <Link to="#" className="hover:underline"> Privacy </Link>
+                <span className="mx-1"> , </span>
+                <Link to="#" className="hover:underline"> Donate</Link>
+                <span className="mx-1">,</span>
+                <Link to="#" className="hover:underline">Reviews</Link>
+                <span className="mx-1">,</span>
+                <Link to="#" className="hover:underline">FAQ</Link>
+                <span className="mx-1">,</span>
+                <Link to="#" className="no-underline hover:underline">Contacts</Link>
+            </div>
+
+        </aside>
+    )
+}
+
+export default Sidebar
