f33fe33c07f69ff105030180703fff6963fed53a
Shivansh <shivanshawasthi33@gmail.com>
2024-10-19 00:56:22 +0530

    added buttons

--- src/components/SingleApp.jsx
+++ src/components/SingleApp.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { LiaDownloadSolid } from "react-icons/lia";
 import { RxCross2 } from "react-icons/rx";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
 
 const SingleApp = () => {
     const [currentIndex, setCurrentIndex] = useState(0);
@@ -132,20 +133,10 @@ const SingleApp = () => {
                             </div>
                             {/* Slider controls */}
                             <button type="button" className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" onClick={prevSlide}>
-                                <span className="inline-flex items-center justify-center w-10 h-10 rounded-full group-focus:ring-white focus:outline-none">
-                                    <svg className="w-4 h-4 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 1 1 5l4 4" />
-                                    </svg>
-                                    <span className="sr-only">Previous</span>
-                                </span>
+                                <FaAngleLeft className='text-xl' />
                             </button>
                             <button type="button" className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" onClick={nextSlide}>
-                                <span className="inline-flex items-center justify-center w-10 h-10 rounded-full group-focus:ring-white focus:outline-none">
-                                    <svg className="w-4 h-4 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4" />
-                                    </svg>
-                                    <span className="sr-only">Next</span>
-                                </span>
+                                <FaAngleRight className='text-xl' />
                             </button>
                         </div>
                     )}
