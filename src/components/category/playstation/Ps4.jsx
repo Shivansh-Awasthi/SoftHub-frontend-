666ff0140aa3603786d4b952a0b4bcbe67ff870f
Shivansh <shivanshawasthi33@gmail.com>
2024-10-19 04:33:13 +0530

    removed log

--- src/components/category/playstation/Ps4.jsx
+++ src/components/category/playstation/Ps4.jsx
@@ -21,7 +21,6 @@ const Ps4 = () => {
                 },
             });
 
-            console.log(response.data); // Log the response
             setData(response.data.apps);
             setTotalItems(response.data.total || 0); // Ensure totalItems is a number
         } catch (error) {
