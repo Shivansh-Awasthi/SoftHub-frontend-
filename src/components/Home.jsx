ba1213a7bae4236610daaf7e5a1f0c9bd3c5d2c7
Shivansh <shivanshawasthi33@gmail.com>
2024-10-28 18:47:58 +0530

    sorting order changed in mac games category

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -103,7 +103,7 @@ const Home = () => {
                 </div>
 
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-                    {data.slice(0, 8).map((ele) => (
+                    {data.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt)).slice(0, 8).map((ele) => (
                         <Link
                             key={ele._id}
                             to={`/${ele._id}`}
