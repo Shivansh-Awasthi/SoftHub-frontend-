f5165842a0f0baa08ea70e50212c63b0a2fec1c2
Shivansh <shivanshawasthi33@gmail.com>
2025-01-07 18:28:12 +0530

    added dmca page

---
+++ src/components/other pages/Dmca.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Dmca = () => {
+    return (
+        <div className='container ring-2 ring-[#2E2E2E] rounded-xl p-6 xl:w-[65%] w-full bg-[#262626] text-gray-200 mt-5'>
+            <div>
+                <h1 className='text-4xl font-medium p-4 pb-6 hover:underline'>Information for copyright holders</h1>
+            </div>
+            <div>
+                <p>If you are the copyright holder of materials (texts, photographs or other objects of copyright) posted on the Site and/or your rights are in any other way violated by the materials of the Site, please send a corresponding request to the Site Administration to the e-mail address support@toxicgames.in and provide comprehensive evidence of ownership of the exclusive right to the relevant materials (works) and/or other information and data on the essence of the violation of rights.</p>
+                <br />
+
+                <p>Your request will be considered by the Site Administration within 7 (Seven) business days from the date of receipt of the request with comprehensive data and confirmation of the violation of your rights.
+                </p>
+                <br />
+                <p>In accordance with the current norms of the legislation of the Russian Federation, the Site Administration is ready to consider all disputed issues within the framework of the pre-trial settlement procedure.</p>
+                <br />
+            </div>
+        </div>
+    )
+}
+
+export default Dmca
