4129a45cc38022d86f75418df0b5902b6fcac3dc
Shivansh <shivanshawasthi33@gmail.com>
2025-03-26 02:18:23 +0530

    added a prop to hide search bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { CiSearch } from "react-icons/ci";
 import { useNavigate } from 'react-router-dom';
 import { RxCross2 } from "react-icons/rx";
 
-const Header = () => {
+const Header = ({ showSearchBar = true }) => {
     const [searchQuery, setSearchQuery] = useState('');
     const navigate = useNavigate();
 
@@ -18,29 +18,31 @@ const Header = () => {
 
     return (
         <header className="flex flex-wrap items-center justify-between px-2 pb-6">
-            <div className="flex flex-wrap relative border border-white border-opacity-5 rounded-lg w-full max-w-[730px]"> {/* Adjusted width */}
-                <form onSubmit={handleSearch} className="w-full flex items-center">
-                    <input
-                        type="text"
-                        placeholder="Search the site"
-                        value={searchQuery}
-                        onChange={(e) => setSearchQuery(e.target.value)}
-                        className="bg-[#242424] hover:bg-[#262626] rounded-lg text-white py-3 pl-4 pr-12 h-12 flex-grow focus:outline-none focus:ring-1 focus:ring-blue-500 focus:bg-opacity-80 transition duration-200"
-                    />
-                    {searchQuery && ( // Show the clear icon only if there is text in the input field.
-                        <button
-                            type="button"
-                            onClick={handleClear}
-                            className="absolute right-14 top-0 h-full w-4 flex items-center justify-center rounded-full"
-                        >
-                            <RxCross2 className="text-xxl h-7 w-7 text-[#8e8e8e] hover:text-[#ffffff]" />
+            {showSearchBar && (
+                <div className="flex flex-wrap relative border border-white border-opacity-5 rounded-lg w-full max-w-[730px]">
+                    <form onSubmit={handleSearch} className="w-full flex items-center">
+                        <input
+                            type="text"
+                            placeholder="Search the site"
+                            value={searchQuery}
+                            onChange={(e) => setSearchQuery(e.target.value)}
+                            className="bg-[#242424] hover:bg-[#262626] rounded-lg text-white py-3 pl-4 pr-12 h-12 flex-grow focus:outline-none focus:ring-1 focus:ring-blue-500 focus:bg-opacity-80 transition duration-200"
+                        />
+                        {searchQuery && (
+                            <button
+                                type="button"
+                                onClick={handleClear}
+                                className="absolute right-14 top-0 h-full w-4 flex items-center justify-center rounded-full"
+                            >
+                                <RxCross2 className="text-xxl h-7 w-7 text-[#8e8e8e] hover:text-[#ffffff]" />
+                            </button>
+                        )}
+                        <button type="submit" className="absolute right-1 top-0 h-full w-12 flex items-center justify-center rounded-full">
+                            <CiSearch className="text-xxl h-7 w-7 text-[#8e8e8e] hover:text-[#ffffff]" />
                         </button>
-                    )}
-                    <button type="submit" className="absolute right-1 top-0 h-full w-12 flex items-center justify-center rounded-full">
-                        <CiSearch className="text-xxl h-7 w-7 text-[#8e8e8e] hover:text-[#ffffff]" />
-                    </button>
-                </form>
-            </div>
+                    </form>
+                </div>
+            )}
         </header>
     );
 };
